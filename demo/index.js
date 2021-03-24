7f9c3c3589d4a497e4190151f30bed9e286662b0
Clément DOUIN <soywod@users.noreply.github.com>
2021-03-24 23:40:46 +0100

    refactor demo

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,16 @@
 const pinFieldJumbotron = document.getElementById("pin-field-jumbotron");
+
+pinFieldJumbotron.addEventListener("complete", () => {
+  pinFieldJumbotron.inputs.forEach(input => (input.disabled = true));
+});
+
 pinFieldJumbotron.format = key => key.toUpperCase();
 pinFieldJumbotron.onComplete = function () {
   this.inputs.forEach(input => (input.disabled = true));
 };
+
+const pinFieldWithEvts = document.getElementById("pin-field-with-evts");
+pinFieldWithEvts.addEventListener("change", evt => alert(`PIN Field changed: ${evt.detail.value}`));
+pinFieldWithEvts.addEventListener("complete", evt => alert(`PIN Field completed: ${evt.detail.value}`));
+pinFieldWithEvts.addEventListener("resolve", evt => alert(`PIN Field key resolved: ${evt.detail.key}`));
+pinFieldWithEvts.addEventListener("reject", evt => alert(`PIN Field key resolved: ${evt.detail.key}`));

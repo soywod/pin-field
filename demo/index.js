79095e4d7b4060d728676e62e10528e470dfc248
Clément DOUIN <soywod@users.noreply.github.com>
2021-03-25 01:36:46 +0100

    fix online demo

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,12 @@
-let field = document.getElementById("pin-field-jumbotron");
-field.format = key => key.toUpperCase();
-field.addEventListener("complete", () => {
-  field.inputs.forEach(input => (input.disabled = true));
-});
+const field1 = document.getElementById("pin-field-jumbotron");
+field1.format = key => key.toUpperCase();
+field1.addEventListener("complete", () => field1.inputs.forEach(input => (input.disabled = true)));
 
-field = document.getElementById("pin-field-uppercase");
-field.format = key => key.toUpperCase();
+const field2 = document.getElementById("pin-field-uppercase");
+field2.format = key => key.toUpperCase();
 
-field = document.getElementById("pin-field-with-evts");
-field.addEventListener("change", evt => alert(`PIN Field changed: ${evt.detail.value}`));
-field.addEventListener("complete", evt => alert(`PIN Field completed: ${evt.detail.value}`));
-field.addEventListener("resolve", evt => alert(`PIN Field key resolved: ${evt.detail.key}`));
-field.addEventListener("reject", evt => alert(`PIN Field key rejected: ${evt.detail.key}`));
+const field3 = document.getElementById("pin-field-with-evts");
+field3.addEventListener("change", evt => alert(`PIN Field changed: ${evt.detail.value}`));
+field3.addEventListener("complete", evt => alert(`PIN Field completed: ${evt.detail.value}`));
+field3.addEventListener("resolve", evt => alert(`PIN Field key resolved: ${evt.detail.key}`));
+field3.addEventListener("reject", evt => alert(`PIN Field key rejected: ${evt.detail.key}`));

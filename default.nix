94dda13348e0db08bdb5ab7133cc4a390cfc4081
Clément DOUIN <clement.douin@posteo.net>
2022-02-10 09:57:55 +0100

    add nix support

---
+++ default.nix
@@ -0,0 +1,11 @@
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  tarball = fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  };
+  lib = import tarball {
+    src = ./.;
+  };
+in
+  lib.defaultNix
